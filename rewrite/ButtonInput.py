42fec3a1a52718d66378c3c4832641eb62c77a8c
Adam Bowes <skylinegames@hotmail.co.uk>
2022-04-18 18:36:02 +0100

    fixed duplicate naming

--- rewrite/ButtonInput.py
+++ rewrite/ButtonInput.py
@@ -14,14 +14,14 @@ class ButtonInput:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.buttonOnePin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.buttonTwoPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.buttonOnePin, GPIO.RISING, callback=self.buttonOneEvent, bouncetime=300) 
-        GPIO.add_event_detect(self.buttonTwoPin, GPIO.RISING, callback=self.buttonTwoEvent, bouncetime=300) 
+        GPIO.add_event_detect(self.buttonOnePin, GPIO.RISING, callback=self.buttonOneCallback, bouncetime=300) 
+        GPIO.add_event_detect(self.buttonTwoPin, GPIO.RISING, callback=self.buttonTwoCallback, bouncetime=300) 
 
-    def buttonOneEvent(self, channel):
+    def buttonOneCallback(self, channel):
         print("btn_1")
         self.buttonOneEvent()
 
-    def buttonTwoEvent(self, channel):
+    def buttonTwoCallback(self, channel):
         print("btn_2")
         self.buttonTwoEvent()
 
