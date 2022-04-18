00bbe54e56ecbcf855156e0199506a701dc7f291
Adam Bowes <skylinegames@hotmail.co.uk>
2022-04-18 18:59:31 +0100

    fix selected rect

--- rewrite/Screen.py
+++ rewrite/Screen.py
@@ -58,7 +58,7 @@ class Screen:
         
         for line in lines:
             if(line.selected):
-                self.draw.rectangle((self.x, self.top + offset,self.width,self.top + (2*offset)), outline=0, fill=255)
+                self.draw.rectangle((self.x, self.top + offset, self.width,self.top + offset + 8), outline=0, fill=255)
             self.draw.text((self.x, self.top + offset), line.text, font=self.font, fill=0 if line.selected else 255)
             offset += 8
 
