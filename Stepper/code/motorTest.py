0cfb4eb17c24d9e90d533df83aa3168fdb1cdc0e
Adam Bowes <skylinegames@hotmail.co.uk>
2023-08-05 18:42:01 +0100

    added test file

---
+++ Stepper/code/motorTest.py
@@ -0,0 +1,23 @@
+from Pins import Pins
+from RPi import GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+pins = Pins()
+
+#GPIO.setup(pins.step.get(), GPIO.OUT)
+GPIO.setup(pins.dir.get(), GPIO.OUT)
+GPIO.setup(pins.enable.get(), GPIO.OUT)
+GPIO.setup(pins.sleep.get(), GPIO.OUT)
+GPIO.setup(pins.reset.get(), GPIO.OUT)
+GPIO.output(pins.enable.get(), 1)
+GPIO.output(pins.sleep.get(), 1)
+GPIO.output(pins.reset.get(), 1)
+
+p = GPIO.PWM(pins.step.get(), 1000)
+p.start(50)
+
+time.sleep(10)
+
+GPIO.cleanup()
