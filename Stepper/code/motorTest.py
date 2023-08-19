1c2a67797084e0c2caa171c195cb9a34354b55ec
Adam Bowes <skylinegames@hotmail.co.uk>
2023-08-19 18:28:34 +0100

    stepper driver test

--- Stepper/code/motorTest.py
+++ Stepper/code/motorTest.py
@@ -1,13 +1,15 @@
 from Pins import Pins
 from RPi import GPIO
 import time
+from rpi_python_drv8825.stepper import StepperMotor
 
 GPIO.setmode(GPIO.BCM)
 
 pins = Pins()
 
-GPIO.setup(pins.step.get(), GPIO.OUT)
-GPIO.setup(pins.dir.get(), GPIO.OUT)
+motor = StepperMotor(pins.enable.get(), pins.step.get(), pins.dir.get(), (pins.m0.get(), pins.m1.get(), pins.m2.get()), 'Full', 0.005)
+
+
 GPIO.setup(pins.enable.get(), GPIO.OUT)
 GPIO.setup(pins.sleep.get(), GPIO.OUT)
 GPIO.setup(pins.reset.get(), GPIO.OUT)
@@ -15,9 +17,11 @@ GPIO.output(pins.enable.get(), 1)
 GPIO.output(pins.sleep.get(), 1)
 GPIO.output(pins.reset.get(), 1)
 
-p = GPIO.PWM(pins.step.get(), 100)
-p.start(50)
+motor.enable(True)        # enables stepper driver
+motor.run(640, True)     # run motor 6400 steps clowckwise
+motor.run(640, False)    # run motor 6400 steps counterclockwise
+motor.enable(False) 
 
-time.sleep(10)
+GPIO.output(pins.sleep.get(), 0)
 
 GPIO.cleanup()
