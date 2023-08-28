8bbbff018d2622a0ae316bdba6070873c1812db4
Adam Bowes <skylinegames@hotmail.co.uk>
2023-08-28 20:33:42 +0100

    added old main

---
+++ Stepper/code/main2.py
@@ -0,0 +1,33 @@
+from CurtainState import CurtainState
+from Pins import Pins
+from RPi import GPIO
+from MotorController import MotorController
+from Screen import Screen
+from threading import Thread
+import time
+from PhysicalInput import PhysicalInput
+
+GPIO.setmode(GPIO.BCM)
+
+pins = Pins()
+state = CurtainState()
+physicalInput = PhysicalInput(pins.rotaryA.get(), pins.rotaryB.get(), pins.rotaryButton.get(), pins.btnL.get(), pins.btnR.get())
+motorController = MotorController(state, pins.step.get(), pins.dir.get(), pins.enable.get(), pins.m0.get(), pins.m1.get(), pins.m2.get(), pins.reset.get(), pins.sleep.get())
+screen = Screen()
+
+def motorLoop():
+    while True:
+        motorController.update()
+
+def controlLoop():
+    while True:
+        state.setTargetPosition(physicalInput.getRotaryPos()*state.stepMultiplier)
+        screen.line2 = str(state.position)
+        time.sleep(0.01)
+
+
+motorThread = Thread(target = motorLoop)
+controlThread = Thread(target = controlLoop)
+
+motorThread.start()
+controlThread.start()
